d3684a96c4161d3379050b4aa9d1ba6c23a12fa3
Alisson Noli <despi.663@gmail.com>
2025-01-27 22:23:38 +0000

    feat(role): add docstring for create_role endpoint to clarify request and response

--- src/controllers/role.py
+++ src/controllers/role.py
@@ -8,6 +8,20 @@ app = Blueprint("role", __name__, url_prefix="/roles")
 
 @app.route('/', methods=['POST'])
 def create_role():
+    """
+    Create a new role.
+
+    This endpoint allows for the creation of a new role by providing the role name in the request body.
+
+    Request Body:
+    {
+        "name": "string"  # The name of the role to be created
+    }
+
+    Returns:
+        dict: A message indicating the role was created successfully.
+        HTTPStatus: The HTTP status code indicating the result of the operation.
+    """
     data = request.json
     role = Role(name=data["name"])
     
