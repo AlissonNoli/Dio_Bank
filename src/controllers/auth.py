e2d0cb50a18229f7f68682bf3b87bc99b0f35a1a
Alisson Noli <despi.663@gmail.com>
2025-01-27 22:23:27 +0000

    feat(auth, role): add detailed docstrings for login and create_role endpoints

--- src/controllers/auth.py
+++ src/controllers/auth.py
@@ -9,6 +9,17 @@ app = Blueprint("auth", __name__, url_prefix="/auth")
 
 @app.route('/login', methods=['POST'])
 def login():
+    """
+    Handle user login and return a JWT access token.
+
+    This endpoint expects a JSON payload with 'username' and 'password'.
+    If the credentials are valid, it returns a JWT access token.
+    Otherwise, it returns an error message with HTTP status 401.
+
+    Returns:
+        dict: A dictionary containing the access token or an error message.
+        HTTPStatus: The HTTP status code.
+    """
     username = request.json.get('username')
     password = request.json.get('password')
     user = db.session.execute(db.select(User).where(User.username == username)).scalar()
