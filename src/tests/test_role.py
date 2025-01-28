fbfaaf72065132e0b57f2621553bbdccf53f4c31
Alisson Noli <despi.663@gmail.com>
2025-01-28 21:04:48 +0000

    test(role): configure test client to use in-memory database

--- src/tests/test_role.py
+++ src/tests/test_role.py
@@ -4,8 +4,12 @@ from src.app import create_app, db, Role
 
 @pytest.fixture
 def client():
-    app = create_app()
-    app.config['TESTING'] = True
+    app = create_app({
+        'TESTING': True,
+        'SQLALCHEMY_DATABASE_URI': 'sqlite:///:memory:',
+        'SQLALCHEMY_TRACK_MODIFICATIONS': False,
+    })
+    
     with app.test_client() as client:
         with app.app_context():
             db.create_all()
