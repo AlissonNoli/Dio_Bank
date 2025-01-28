a9df9ff7b413539ae1e076b4e0b7789236182cc6
Alisson Noli <despi.663@gmail.com>
2025-01-28 21:04:18 +0000

    test(conftest): update test client fixture to use in-memory database

--- conftest.py
+++ conftest.py
@@ -3,24 +3,29 @@ import os
 import pytest
 from src.app import create_app, db
 
-@pytest.fixture
+@pytest.fixture(scope='module')
+def test_client():
+    flask_app = create_app({
+        'TESTING': True,
+        'SQLALCHEMY_DATABASE_URI': 'sqlite:///:memory:'
+    })
+
+    # Create a test client using the Flask application configured for testing
+    with flask_app.test_client() as testing_client:
+        with flask_app.app_context():
+            db.create_all()
+            yield testing_client  # this is where the testing happens!
+            db.drop_all()
+
+@pytest.fixture(scope='module')
 def app():
     app = create_app({
         'TESTING': True,
-        'SQLALCHEMY_DATABASE_URI': 'sqlite:///test_blog.sqlite',
+        'SQLALCHEMY_DATABASE_URI': 'sqlite:///:memory:'
     })
-
     with app.app_context():
         db.create_all()
         yield app
         db.drop_all()
-
-@pytest.fixture
-def client(app):
-    return app.test_client()
-
-@pytest.fixture
-def runner(app):
-    return app.test_cli_runner()
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
