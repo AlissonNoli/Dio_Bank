431197ecfe6d6a6e0ec43db366902d3af3483eb7
Alisson Noli <despi.663@gmail.com>
2025-01-28 21:04:54 +0000

    test(utils): add tests for requires_roles decorator functionality

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -1,5 +1,6 @@
 import pytest
-from src.utils import eleva_quadrado
+from src.utils import eleva_quadrado, requires_roles
+from http import HTTPStatus
 
 @pytest.mark.parametrize("entrada, esperado", [(2, 4), (3, 9), (4, 16), (0, 0), (-2, 4)])
 def test_eleva_quadrado_sucesso(entrada, esperado):
@@ -16,3 +17,33 @@ def test_eleva_quadrado_erro(entrada, exc_class, msg):
     with pytest.raises(exc_class) as exc:
         eleva_quadrado(entrada)
     assert str(exc.value) == msg
+
+def test_requires_roles_success(mocker):
+    # Given
+    mock_user = mocker.Mock()
+    mock_user.role.name = 'admin'
+    
+    mocker.patch('src.utils.get_jwt_identity')
+    mocker.patch('src.utils.db.get_or_404', return_value=mock_user)
+    decorated_function = requires_roles('admin')(lambda: "success")            
+
+    # When    
+    result = decorated_function()
+    
+    # Then
+    assert result == "success"
+        
+def test_requires_roles_fail(mocker):
+    # Given
+    mock_user = mocker.Mock()
+    mock_user.role.name = 'normal'
+    
+    mocker.patch('src.utils.get_jwt_identity'), 
+    mocker.patch('src.utils.db.get_or_404', return_value=mock_user)
+    decorated_function = requires_roles('admin')(lambda: "success")            
+    
+    # When
+    result = decorated_function()
+
+    # Then
+    assert result == ({"msg": "Admin only!"}, HTTPStatus.FORBIDDEN)
