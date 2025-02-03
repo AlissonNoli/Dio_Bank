460226538023026225ff8188a55ad3187dd7a1f9
Alisson Noli <despi.663@gmail.com>
2025-02-03 14:55:19 +0000

    refactor(tests): simplify test client setup and remove unused code

--- conftest.py
+++ conftest.py
@@ -3,21 +3,7 @@ import os
 import pytest
 from src.app import create_app, db
 
-@pytest.fixture(scope='module')
-def test_client():
-    flask_app = create_app({
-        'TESTING': True,
-        'SQLALCHEMY_DATABASE_URI': 'sqlite:///:memory:'
-    })
-
-    # Create a test client using the Flask application configured for testing
-    with flask_app.test_client() as testing_client:
-        with flask_app.app_context():
-            db.create_all()
-            yield testing_client  # this is where the testing happens!
-            db.drop_all()
-
-@pytest.fixture(scope='module')
+@pytest.fixture()
 def app():
     app = create_app({
         'TESTING': True,
@@ -26,6 +12,26 @@ def app():
     with app.app_context():
         db.create_all()
         yield app
-        db.drop_all()
+        
+        # db.drop_all()
+        
+# @pytest.fixture(scope='module')
+# def test_client():
+#     flask_app = create_app({
+#         'TESTING': True,
+#         'SQLALCHEMY_DATABASE_URI': 'sqlite:///:memory:'
+#     })
+
+#     with flask_app.test_client() as testing_client:
+#         with flask_app.app_context():
+#             db.create_all()
+            
+#             yield testing_client  
+#             db.drop_all()
+            
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
